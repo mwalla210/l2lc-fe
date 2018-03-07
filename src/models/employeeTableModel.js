6d166245f5e9fb104f762264c10100f20c551198
Morgan Wallace <mlw343@drexel.edu>
2018-03-07 17:21:43 -0500

    Changed to make use of center classname instead of modifying width

--- src/models/employeeTableModel.js
+++ src/models/employeeTableModel.js
@@ -54,7 +54,11 @@ export default class EmployeeTableModel extends TableModel{
       {
         Header: 'Barcode',
         accessor: 'barcode',
-        maxWidth: 240,
+        getProps: () => {
+          return {
+            className: 'center',
+          }
+        },
         Cell: row => (
           <span>
             <span>
