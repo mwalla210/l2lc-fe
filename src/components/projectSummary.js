6d1815fc9253ff9b587626f489ee9f6a00a8c77d
Morgan Wallace <mlw343@drexel.edu>
2018-03-08 15:12:48 -0500

    Printing capabilities on project summary

--- src/components/projectSummary.js
+++ src/components/projectSummary.js
@@ -43,7 +43,8 @@ export default class ProjectSummary extends Component {
   }
 
   printClick(){
-    console.log('Print')
+    // eslint-disable-next-line no-undef
+    window.print()
   }
 
   timeEntries(){
