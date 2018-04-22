71a69a66c0bd16877c3f39dad445a4c36ee8f476
lgw28 <lgw28@cs.drexel.edu>
2018-04-22 18:26:33 -0400

    selectField test half done, not sure how to write the options for a mobx array of objects

---
+++ src/tests/components/selectField.test.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import SelectField from '../../components/selectField'
+import {observable} from 'mobx'
+
+const defaultOptions = {
+  valid: true,
+  disabled: false,
+  id: 'id',
+  value: 'value',
+  onChange: jest.fn(),
+  onBlur: jest.fn(),
+  focus: true,
+  options:observable([])
+}
+
+const alternateOptions = {
+  valid: false,
+  disabled: false,
+  id: 'id',
+  value: 'value',
+  onChange: jest.fn(),
+  onBlur: jest.fn(),
+  focus: true,
+  options:observable([])
+}
+
+describe('SelectField', () => {
+  it ('Renders with snapshot', () => {
+    let options = Object.assign({}, defaultOptions)
+    const component = renderer.create(
+      <SelectField {...options}/>,
+    )
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it ('Renders with snapshot, alternate arguements', () => {
+    let options = Object.assign({}, alternateOptions)
+    const component = renderer.create(
+      <SelectField {...options}/>,
+    )
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+})
