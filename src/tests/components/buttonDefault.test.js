1c0518f80e8b653e936bb58408966f1ddfefb3fa
Denissa Pribec <dap334@drexel.edu>
2018-04-23 01:04:56 -0400

    all of my tests pushed except for FieldModal

---
+++ src/tests/components/buttonDefault.test.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import ButtonDefault from '../../components/buttonDefault'
+
+const defaultOptions = {
+  onClick: jest.fn(),
+  disabled: false,
+  text: 'text',
+  className: 'className',
+  type: 'button',
+  style: null
+}
+
+describe('ButtonDefault', () => {
+  it ('Renders with snapshot (disabled: false)', () => {
+    let options = Object.assign({}, defaultOptions)
+    const component = renderer.create(
+      <ButtonDefault {...options}/>,
+    )
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+  it ('Renders with snapshot (disabled: true)', () => {
+    let options = Object.assign({}, defaultOptions)
+    options.disabled = true
+    const component = renderer.create(
+      <ButtonDefault {...options}/>,
+    )
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+  it ('Renders with snapshot (disabled: true, className: empty string)', () => {
+    let options = Object.assign({}, defaultOptions)
+    options.className = ''
+    const component = renderer.create(
+      <ButtonDefault {...options}/>,
+    )
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
---
+++ src/tests/components/buttonPrimary.test.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import ButtonPrimary from '../../components/buttonPrimary'
+jest.mock('../../components/buttonDefault')
+
+const defaultOptions = {
+  onClick: jest.fn(),
+  disabled: false,
+  text: 'text',
+  className: 'className',
+  type: 'primary',
+  style: null
+}
+
+describe('ButtonPrimary', () => {
+  it ('Renders with snapshot (disabled: false)', () => {
+    let options = Object.assign({}, defaultOptions)
+    const component = renderer.create(
+      <ButtonPrimary {...options}/>,
+    )
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+  it ('Renders with snapshot (disabled: true)', () => {
+    let options = Object.assign({}, defaultOptions)
+    options.disabled = true
+    const component = renderer.create(
+      <ButtonPrimary {...options}/>,
+    )
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+  it ('Renders with snapshot (disabled: true, className: empty string)', () => {
+    let options = Object.assign({}, defaultOptions)
+    options.className = ''
+    const component = renderer.create(
+      <ButtonPrimary {...options}/>,
+    )
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+  it ('Renders with snapshot (disabled: false, className: empty string)', () => {
+    let options = Object.assign({}, defaultOptions)
+    options.className = ''
+    const component = renderer.create(
+      <ButtonPrimary {...options}/>,
+    )
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
