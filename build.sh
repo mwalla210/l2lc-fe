6e208409d0f378de15f9feec07a4c9ea23c072b1
Morgan Wallace <mlw343@drexel.edu>
2018-04-22 23:56:29 -0400

    Updated build script to exclude tests, coverage

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # Construct archive
 npm run build
-zip -r archive * -x \*coverage\* \*node_modules\* \*custom\ docdash\* \*doc\* \*build\* \*plugins\*
+zip -r archive * -x \*coverage\* \*tests\* \*node_modules\* \*custom\ docdash\* \*doc\* \*build\* \*plugins\*
 rm bundle.js
 # ssh casuman07@107.180.27.180 "cd public_html/cloud/; find * -not -name 'cgi-bin' -delete; exit"
 # rm archive.zip
