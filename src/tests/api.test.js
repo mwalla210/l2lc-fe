f2c54f46545e13e2413cef38a1de69f889e6d423
Morgan Wallace <mlw343@drexel.edu>
2018-04-30 23:11:02 -0400

    Customer set of function tests finished

--- src/tests/api.test.js
+++ src/tests/api.test.js
@@ -25,6 +25,13 @@ nock('http://138.197.88.198:8080/l2lc/api')
           country: 'country',
           zip: 'zip',
           street: 'street',
+        },
+        billingAddr: {
+          city: 'city',
+          state: 'state',
+          country: 'country',
+          zip: 'zip',
+          street: 'street, apartment',
         }
       },
     ]
@@ -33,6 +40,13 @@ nock('http://138.197.88.198:8080/l2lc/api')
   .reply(200, {
     id: '1',
     shippingAddr: {
+      city: 'city',
+      state: 'state',
+      country: 'country',
+      zip: 'zip',
+      street: 'street',
+    },
+    billingAddr: {
       city: 'city',
       state: 'state',
       country: 'country',
@@ -51,9 +65,46 @@ nock('http://138.197.88.198:8080/l2lc/api')
       street: 'street',
     }
   })
+  .post('/customer/create', body => {return body.duplicate})
+  .reply(406)
+  .post('/customer/create', body => {return body.unexpected})
+  .reply(500)
+  .post('/customer/create', body => {return body.success})
+  .reply(201, {
+    id: '1',
+    shippingAddr: {
+      city: 'city',
+      state: 'state',
+      country: 'country',
+      zip: 'zip',
+      street: 'street, apartment',
+    },
+    billingAddr: {
+      city: 'city',
+      state: 'state',
+      country: 'country',
+      zip: 'zip',
+      street: 'billstreet',
+    }
+  })
+  .post('/customer/1/update', () => {return true})
+  .reply(406)
+  .post('/customer/2/update', () => {return true})
+  .reply(200, {
+    id: '1',
+    shippingAddr: {
+      city: 'city',
+      state: 'state',
+      country: 'country',
+      zip: 'zip',
+      street: 'street',
+    }
+  })
+  .post('/customer/3/update', () => {return true})
+  .reply(500)
 
 describe('API', () => {
-  it ('Tests fetchCustomers (matching ID)', async function() {
+  it ('Tests fetchCustomers', async function() {
     let response = await API.fetchCustomers()
     expect(response).toHaveLength(2)
     response.forEach(item => {
@@ -67,5 +118,41 @@ describe('API', () => {
   it ('Tests fetchCustomer (non-matching ID)', async function() {
     let response = await API.fetchCustomer(2)
     expect(response).toBeNull()
+  })
+  it ('Tests createCustomer (duplicate)', async function() {
+    let response = await API.createCustomer(JSON.stringify({
+      duplicate: true
+    }))
+    expect(response).toBe('Duplicate entry exists')
+  })
+  it ('Tests createCustomer (success)', async function() {
+    let response = await API.createCustomer(JSON.stringify({
+      success: true
+    }))
+    expect(response).toBeInstanceOf(CustomerModel)
+  })
+  it ('Tests createCustomer (unexpected error)', async function() {
+    let response = await API.createCustomer(JSON.stringify({
+      unexpected: true
+    }))
+    expect(response).toBe('Unexpected error 500')
+  })
+  it ('Tests updateCustomer (duplicate)', async function() {
+    let response = await API.updateCustomer(1,JSON.stringify({
+      duplicate: true
+    }))
+    expect(response).toBe('Duplicate entry exists')
+  })
+  it ('Tests updateCustomer (success)', async function() {
+    let response = await API.updateCustomer(2,JSON.stringify({
+      success: true
+    }))
+    expect(response).toBeInstanceOf(CustomerModel)
+  })
+  it ('Tests updateCustomer (unexpected error)', async function() {
+    let response = await API.updateCustomer(3,JSON.stringify({
+      unexpected: true
+    }))
+    expect(response).toBe('Unexpected error 500')
   })
 })
