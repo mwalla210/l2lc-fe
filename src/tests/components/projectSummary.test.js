289f091f6fcfaa2024733d18206e5829db678220
Morgan Wallace <mlw343@drexel.edu>
2018-05-04 00:29:49 -0400

    Somewhat unrelated, but dropped all mention of rework and hold for projects and associated data/structures/logic

--- src/tests/components/projectSummary.test.js
+++ src/tests/components/projectSummary.test.js
@@ -3,7 +3,6 @@ import renderer from 'react-test-renderer'
 import ProjectSummary from '../../components/projectSummary'
 jest.mock('../../components/barcode')
 jest.mock('../../components/deleteModal')
-jest.mock('../../components/fieldModal')
 jest.mock('../../components/promptModal')
 jest.mock('../../components/buttonDefault')
 jest.mock('reactstrap', () => ({
@@ -17,17 +16,6 @@ jest.mock('reactstrap', () => ({
 const defaultOptions = {
   page: {
     summaryModel: {
-      fieldModal: {
-        changeTitle: jest.fn(),
-        changeConfirmFn: jest.fn(),
-        openModal: jest.fn(),
-        title: 'fieldModal',
-        confirmAndClose: jest.fn(),
-        modalOpen: false,
-        closeModal: jest.fn(),
-        changeContent: jest.fn(),
-        contents: 'contents'
-      },
       deleteModal: {
         openModal: jest.fn(),
         confirmAndClose: jest.fn(),
@@ -103,76 +91,6 @@ describe('ProjectSummary', () => {
     )
     let tree = component.toJSON()
     expect(tree).toMatchSnapshot()
-  })
-  it ('Renders and calls reworkClick', () => {
-    let options = Object.assign({}, defaultOptions)
-    const component = renderer.create(
-      <ProjectSummary {...options}/>,
-    )
-    const inst = component.getInstance()
-    expect(inst.props.page.summaryModel.fieldModal.changeTitle.mock.calls.length).toBe(0)
-    expect(inst.props.page.summaryModel.fieldModal.changeConfirmFn.mock.calls.length).toBe(0)
-    expect(inst.props.page.summaryModel.fieldModal.changeContent.mock.calls.length).toBe(0)
-    expect(inst.props.page.summaryModel.fieldModal.openModal.mock.calls.length).toBe(0)
-    inst.reworkClick()
-    expect(inst.props.page.summaryModel.fieldModal.changeTitle.mock.calls.length).toBe(1)
-    expect(inst.props.page.summaryModel.fieldModal.changeTitle.mock.calls[0][0]).toBe('Add Rework')
-    expect(inst.props.page.summaryModel.fieldModal.changeConfirmFn.mock.calls.length).toBe(1)
-    expect(typeof inst.props.page.summaryModel.fieldModal.changeConfirmFn.mock.calls[0][0]).toBe('function')
-    inst.props.page.summaryModel.fieldModal.changeConfirmFn.mock.calls[0][0]()
-    expect(inst.props.page.summaryModel.fieldModal.changeContent.mock.calls.length).toBe(1)
-    expect(inst.props.page.summaryModel.fieldModal.changeContent.mock.calls[0][0]).toBe('')
-    expect(inst.props.page.summaryModel.fieldModal.openModal.mock.calls.length).toBe(1)
-  })
-  it ('Renders and calls holdClick', () => {
-    let options = Object.assign({}, defaultOptions)
-    options.page.summaryModel.fieldModal.changeTitle = jest.fn()
-    options.page.summaryModel.fieldModal.changeConfirmFn = jest.fn()
-    options.page.summaryModel.fieldModal.changeContent = jest.fn()
-    options.page.summaryModel.fieldModal.openModal = jest.fn()
-    options.website.currentProject.hold.flag = false
-    const component = renderer.create(
-      <ProjectSummary {...options}/>,
-    )
-    const inst = component.getInstance()
-    expect(inst.props.page.summaryModel.fieldModal.changeTitle.mock.calls.length).toBe(0)
-    expect(inst.props.page.summaryModel.fieldModal.changeConfirmFn.mock.calls.length).toBe(0)
-    expect(inst.props.page.summaryModel.fieldModal.changeContent.mock.calls.length).toBe(0)
-    expect(inst.props.page.summaryModel.fieldModal.openModal.mock.calls.length).toBe(0)
-    inst.holdClick()
-    expect(inst.props.page.summaryModel.fieldModal.changeTitle.mock.calls.length).toBe(1)
-    expect(inst.props.page.summaryModel.fieldModal.changeTitle.mock.calls[0][0]).toBe('Add Hold')
-    expect(inst.props.page.summaryModel.fieldModal.changeConfirmFn.mock.calls.length).toBe(1)
-    expect(typeof inst.props.page.summaryModel.fieldModal.changeConfirmFn.mock.calls[0][0]).toBe('function')
-    inst.props.page.summaryModel.fieldModal.changeConfirmFn.mock.calls[0][0]()
-    expect(inst.props.page.summaryModel.fieldModal.changeContent.mock.calls.length).toBe(1)
-    expect(inst.props.page.summaryModel.fieldModal.changeContent.mock.calls[0][0]).toBe('')
-    expect(inst.props.page.summaryModel.fieldModal.openModal.mock.calls.length).toBe(1)
-  })
-  it ('Renders and calls holdClick', () => {
-    let options = Object.assign({}, defaultOptions)
-    options.page.summaryModel.fieldModal.changeTitle = jest.fn()
-    options.page.summaryModel.fieldModal.changeConfirmFn = jest.fn()
-    options.page.summaryModel.fieldModal.changeContent = jest.fn()
-    options.page.summaryModel.fieldModal.openModal = jest.fn()
-    options.website.currentProject.hold.flag = true
-    const component = renderer.create(
-      <ProjectSummary {...options}/>,
-    )
-    const inst = component.getInstance()
-    expect(inst.props.page.summaryModel.fieldModal.changeTitle.mock.calls.length).toBe(0)
-    expect(inst.props.page.summaryModel.fieldModal.changeConfirmFn.mock.calls.length).toBe(0)
-    expect(inst.props.page.summaryModel.fieldModal.changeContent.mock.calls.length).toBe(0)
-    expect(inst.props.page.summaryModel.fieldModal.openModal.mock.calls.length).toBe(0)
-    inst.holdClick()
-    expect(inst.props.page.summaryModel.fieldModal.changeTitle.mock.calls.length).toBe(1)
-    expect(inst.props.page.summaryModel.fieldModal.changeTitle.mock.calls[0][0]).toBe('Remove Hold')
-    expect(inst.props.page.summaryModel.fieldModal.changeConfirmFn.mock.calls.length).toBe(1)
-    expect(typeof inst.props.page.summaryModel.fieldModal.changeConfirmFn.mock.calls[0][0]).toBe('function')
-    inst.props.page.summaryModel.fieldModal.changeConfirmFn.mock.calls[0][0]()
-    expect(inst.props.page.summaryModel.fieldModal.changeContent.mock.calls.length).toBe(1)
-    expect(inst.props.page.summaryModel.fieldModal.changeContent.mock.calls[0][0]).toBe('')
-    expect(inst.props.page.summaryModel.fieldModal.openModal.mock.calls.length).toBe(1)
   })
   it ('Renders and calls tasksClick', () => {
     let options = Object.assign({}, defaultOptions)
--- src/tests/models/projectSummaryModel.test.js
+++ src/tests/models/projectSummaryModel.test.js
@@ -5,7 +5,6 @@ describe('ProjectSummaryModel', () => {
   it ('Tests constructor', () => {
     let modal = new ProjectSummaryModel(jest.fn(), jest.fn())
     expect(modal).toHaveProperty('deleteModal')
-    expect(modal).toHaveProperty('fieldModal')
     expect(modal).toHaveProperty('completeModal')
   })
 })
