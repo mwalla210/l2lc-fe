74fbf935fadd6be54135772019321b08b3dcd3e6
lgw28 <lgw28@cs.drexel.edu>
2018-05-18 20:53:22 -0400

    almost complete timeEntryFormModel

--- src/tests/models/timeEntryFormModel.test.js
+++ src/tests/models/timeEntryFormModel.test.js
@@ -3,20 +3,16 @@ import TimeEntryFormModel from '../../models/timeEntryFormModel'
 
 jest.mock('../../store/website', () => {
   return {
-    createTimeEntry: jest.fn().mockReturnValueOnce(Promise.resolve(null)).mockReturnValueOnce(Promise.resolve('response')),
+    createTimeEntry: jest.fn().mockReturnValue(Promise.resolve('error')),//.mockReturnValueOnce(Promise.resolve(null)).mockReturnValueOnce(Promise.resolve('response'))
     currentUser:{
       stationID: 'Receiving'
-    }
+    },
+    addToTaskHistory: jest.fn(),
   }
 })
 jest.mock('../../store/page', () => {
   return {
-
-  }
-})
-jest.mock('../../consts', () => {
-  return {
-    stationName: jest.fn().mockReturnValueOnce(true)
+    projectTimeEntryMenuItem: jest.fn()
   }
 })
 
@@ -25,7 +21,6 @@ describe('TimeEntryFormModel', () => {
     let entry = new TimeEntryFormModel()
     entry.station = null
     entry.projectID = []
-    console.log(entry.projectID.length)
     entry.enployeeID= []
     expect(entry).toHaveProperty('resetValuesAndValidation')
   })
@@ -34,7 +29,6 @@ describe('TimeEntryFormModel', () => {
     let entry = new TimeEntryFormModel()
     entry.station = null
     entry.projectID = []
-    console.log(entry.projectID.length)
     entry.enployeeID= []
     expect(entry).toHaveProperty('resetValuesAndValidation')
   })
@@ -69,7 +63,7 @@ describe('TimeEntryFormModel', () => {
     expect(entry.value).toEqual('')
   })
 
-  xit ('Tests setValue for project', () => {
+  it ('Tests setValue for project', () => {
     let entry = new TimeEntryFormModel()
     entry.station = 'null'
     entry.projectID = [{replace: jest.fn()},{replace: jest.fn()}]
@@ -78,10 +72,20 @@ describe('TimeEntryFormModel', () => {
     expect(entry.value).toEqual('1121')
   })
 
-  it ('Tests setValue for employee', () => {
+  it ('Tests setValue for e', () => {
     let entry = new TimeEntryFormModel()
+    entry.projectID = [{replace: jest.fn()},{replace: jest.fn()},'E1121']
+    entry.employeeID = [{replace: jest.fn()},{replace: jest.fn()}, 'E1121']
     entry.setValue('E1121','split')
     expect(entry.value).toEqual('E1121')
+  })
+
+  it ('Tests setValue for p', () => {
+    let entry = new TimeEntryFormModel()
+    entry.projectID = [{replace: jest.fn()},{replace: jest.fn()},'P1121']
+    entry.employeeID = [{replace: jest.fn()},{replace: jest.fn()}, 'P1121']
+    entry.setValue('P1121','split')
+    expect(entry.value).toEqual('P1121')
   })
 
   it ('Tests setValue for either', () => {
@@ -90,7 +94,7 @@ describe('TimeEntryFormModel', () => {
     expect(entry.value).toEqual('E1121')
   })
 
-  it ('Tests setValue for neither', () => {
+  it ('Tests setValue for incorrect input', () => {
     let entry = new TimeEntryFormModel()
     entry.setValue('E','')
     expect(entry.value).toEqual('E')
@@ -102,13 +106,33 @@ describe('TimeEntryFormModel', () => {
     expect(entry.value).toEqual(null)
   })
 
-  it ('Tests setValue for incorrect input', () => {
+  it ('Tests setValue shorthand', () => {
+    let entry = new TimeEntryFormModel()
+    entry.setValue('deccoat','deccoat')
+    expect(entry.value).toEqual('deccoat')
+  })
+
+  it ('Tests setValue station', () => {
+    let entry = new TimeEntryFormModel()
+    entry.setValue('receiving','receiving')
+    expect(entry.value).toEqual('receiving')
+  })
+
+  it ('Tests setValue for alterante incorrect input', () => {
     let entry = new TimeEntryFormModel()
     entry.setValue('PEIgf13','dfrgd63')
     expect(entry.value).toEqual('PEIgf13')
   })
 
-  it ('Tests submit for null', () => {
+  it ('Tests submit ', () => {
+    let entry = new TimeEntryFormModel()
+    entry.projectID = [{replace: jest.fn()},{replace: jest.fn()},'E1121']
+    entry.employeeID = [{replace: jest.fn()},{replace: jest.fn()}, 'E1121']
+    entry.submit()
+    expect(entry.errorModalOpen).toEqual(false)
+  })
+
+  it ('Tests submit ', () => {
     let entry = new TimeEntryFormModel()
     entry.submit()
     expect(entry.errorModalOpen).toEqual(false)
